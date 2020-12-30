e65c111e2518f7674a81b28b67799150b113d073
Roberto Margain <margain.roberto@gmail.com>
2020-12-30 00:43:51 -0600

    12/30 0:43

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const app = express();
 //utilizo cors para darle permisos a otras apps
 app.use(
     cors({
-        origin: ['http://localhost:3001', 'https://robert-caro-app.herokuapp.com'],
+        origin: ['http://localhost:3001','https://robert-caro-app.herokuapp.com'],
         credentials: true,
     })
 )
